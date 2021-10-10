e13f1e29dc7e075cfcde27a3ec7df39228471143
marioparrilla <marioparrilla2@gmail.com>
2021-10-10 14:04:37 +0200

    Venanta Carga creada y funcional

--- DI-UT1-Actividad1.1/VentanaCarga.Designer.cs
+++ DI-UT1-Actividad1.1/VentanaCarga.Designer.cs
@@ -29,22 +29,79 @@ namespace DI_UT1_Actividad1._1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timerCarga = new System.Windows.Forms.Timer(this.components);
+            this.pbCarga = new System.Windows.Forms.ProgressBar();
+            this.lblNombreActividad = new System.Windows.Forms.Label();
+            this.lblProgreso = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timerCarga
+            // 
+            this.timerCarga.Enabled = true;
+            this.timerCarga.Tick += new System.EventHandler(this.timerCarga_Tick);
+            // 
+            // pbCarga
+            // 
+            this.pbCarga.Location = new System.Drawing.Point(40, 90);
+            this.pbCarga.Maximum = 101;
+            this.pbCarga.Name = "pbCarga";
+            this.pbCarga.Size = new System.Drawing.Size(252, 27);
+            this.pbCarga.TabIndex = 0;
+            // 
+            // lblNombreActividad
+            // 
+            this.lblNombreActividad.AutoSize = true;
+            this.lblNombreActividad.Location = new System.Drawing.Point(36, 42);
+            this.lblNombreActividad.Name = "lblNombreActividad";
+            this.lblNombreActividad.Size = new System.Drawing.Size(256, 20);
+            this.lblNombreActividad.TabIndex = 1;
+            this.lblNombreActividad.Text = "Actividad 1.1 - Mario Parrilla Maroto";
+            // 
+            // lblProgreso
+            // 
+            this.lblProgreso.AutoSize = true;
+            this.lblProgreso.Location = new System.Drawing.Point(257, 120);
+            this.lblProgreso.Name = "lblProgreso";
+            this.lblProgreso.Size = new System.Drawing.Size(32, 20);
+            this.lblProgreso.TabIndex = 2;
+            this.lblProgreso.Text = "0%";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(45, 120);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(158, 20);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "Iniciando Programa...";
             // 
             // VentanaCarga
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(779, 438);
+            this.ClientSize = new System.Drawing.Size(331, 167);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblProgreso);
+            this.Controls.Add(this.lblNombreActividad);
+            this.Controls.Add(this.pbCarga);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "VentanaCarga";
             this.Text = "Trabajo Tutoriales - Cargando";
             this.Load += new System.EventHandler(this.VentanaCarga_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timerCarga;
+        private System.Windows.Forms.ProgressBar pbCarga;
+        private System.Windows.Forms.Label lblNombreActividad;
+        private System.Windows.Forms.Label lblProgreso;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
--- DI-UT1-Actividad1.1/VentanaCarga.cs
+++ DI-UT1-Actividad1.1/VentanaCarga.cs
@@ -13,9 +13,11 @@ namespace DI_UT1_Actividad1._1
 {
     public partial class VentanaCarga : Form
     {
+        VentanaInicial vi = null;
         public VentanaCarga()
         {
             InitializeComponent();
+            vi = new VentanaInicial();
         }
 
         private void VentanaCarga_Load(object sender, EventArgs e)
@@ -28,8 +30,29 @@ namespace DI_UT1_Actividad1._1
             //Establecemos valores predeterminados de la ventana
             this.MaximizeBox = false;
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
-
             this.Icon = new Icon(Directory.GetCurrentDirectory() + @"/../../Resources/Icons/iconoPrograma.ico");
+
+
+        }
+
+        private void timerCarga_Tick(object sender, EventArgs e)//Funcionalidad
+        {
+            pbCarga.Increment(1);
+            lblProgreso.Text = pbCarga.Value.ToString()+"%";
+
+            //Segun el porcentaje se actualizará el lblInfo
+            if (pbCarga.Value == 30) lblInfo.Text = "Cargando Recursos...";
+            else if (pbCarga.Value == 50) lblInfo.Text = "Cargando Datos...";
+            else if (pbCarga.Value == 70) lblInfo.Text = "Cargando Temas...";
+            else if (pbCarga.Value == 85) lblInfo.Text = "Cargando Tutoriales...";
+            else if (pbCarga.Value == 95) lblInfo.Text = "Iniciando...";
+
+            if (pbCarga.Value == pbCarga.Maximum)//En cuanto llegue al maximo, paramos el timer, ocultamos esta ventana y iniciamos la ventana inicial
+            {
+                timerCarga.Stop();
+                this.Hide();
+                if (vi.ShowDialog() == DialogResult.Cancel) this.Close();//Cuando se cierre la ventana inicial, cerraremos esta que esta oculta
+            }
         }
     }
 }
